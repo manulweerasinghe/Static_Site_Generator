38fdab29c76206a1515ad4f839871963b6406ec3
manulweerasinghe <weerasinghemanul@gmail.com>
2026-04-17 12:24:50 +0530

    add more delimiter tests

--- src/test_split_delimiter.py
+++ src/test_split_delimiter.py
@@ -4,11 +4,34 @@ from split_delimiter import *
 from textnode import *
 
 class TestSplitDilimiter(unittest.TestCase):
-    def test_eq(self):
+    def test_code(self):
         result = split_nodes_delimiter([TextNode("This is text with a `code block` word", TextType.PLAIN_TEXT)], "`", TextType.CODE_TEXT)
         expected = [
         TextNode("This is text with a ", TextType.PLAIN_TEXT),
         TextNode("code block", TextType.CODE_TEXT),
         TextNode(" word", TextType.PLAIN_TEXT),
         ]
+        self.assertEqual(result, expected)
+
+    def test_bold(self):
+        node = TextNode("This is **bolded and bold**, then again **bolded**.", TextType.PLAIN_TEXT)
+        result = split_nodes_delimiter([node], "**", TextType.BOLD_TEXT)
+        expected = [
+                TextNode("This is ", TextType.PLAIN_TEXT),
+                TextNode("bolded and bold", TextType.BOLD_TEXT),
+                TextNode(", then again ", TextType.PLAIN_TEXT),
+                TextNode("bolded", TextType.BOLD_TEXT),
+                TextNode(".", TextType.PLAIN_TEXT),
+                ]
+        self.assertEqual(result, expected)
+
+    def test_bold_and_italic(self):
+        node = TextNode("**bolded** & _italic_", TextType.PLAIN_TEXT)
+        result = split_nodes_delimiter([node], "**", TextType.BOLD_TEXT)
+        result = split_nodes_delimiter(result, "_", TextType.ITALIC_TEXT)
+        expected = [
+                TextNode("bolded", TextType.BOLD_TEXT),
+                TextNode(" & ", TextType.PLAIN_TEXT),
+                TextNode("italic", TextType.ITALIC_TEXT),
+                ]
         self.assertEqual(result, expected)
